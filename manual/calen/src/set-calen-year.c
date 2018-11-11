6f167b6ea06ca3bcc7e6c1ce99f4721525ada7f2
tikubonn <https://twitter.com/tikubonn>
2018-11-11 20:25:55 +0900

    added some codes for adjusting date of day after changed year.

--- manual/calen/src/set-calen-year.c
+++ manual/calen/src/set-calen-year.c
@@ -1,4 +1,5 @@
 #include <calen.private>
+#define min(a,b) ((a)<(b)?(a):(b))
 
 /**
  * @fn
@@ -13,5 +14,7 @@ int set_calen_year (int year, calen *calout){
     return 1;
   }
   calout->year = year;
+  int days = calen_month_of_days(calout->year, calout->month);
+  calout->day = min(calout->day, days);
   return 0;
 }
--- test/manual/calen/src/test-manual-set-calen-year.c
+++ test/manual/calen/src/test-manual-set-calen-year.c
@@ -16,5 +16,13 @@ void test_manual_set_calen_year (){
   test(init_calen(1, 1, 1, 0, 0, 0, &temp) == 0);
   test(set_calen_year(8, &temp) == 0);
   test(temp.year == 8);
-    
+
+  /* <0004/02/29 00:00:00> to <0001/02/28 00:00:00> */
+  
+  test(init_calen(4, 2, 29, 0, 0, 0, &temp) == 0);
+  test(set_calen_year(1, &temp) == 0);
+  test(temp.year == 1);
+  test(temp.month == 2);
+  test(temp.day == 28);
+  
 }
